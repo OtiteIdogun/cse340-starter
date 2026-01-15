7c760b79029249f1e010b5baaa81f96a739b6ab3
Otite Idogun <otiteidogun@gmail.com>
2026-01-15 18:27:06 +0100

    models/inventory-model.js - Add getInventoryById Function
    - Added new function getInventoryById(inv_id) to retrieve a single inventory item by ID from the database.
    - Implements SQL query: SELECT * FROM public.inventory WHERE inv_id = .
    - Exports the new function for external use.

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -142,6 +142,17 @@ checkExistingInventoryItem = async (inv_make, inv_model, inv_year, inv_color, cl
   }
 }
 
+async function getInventoryById(inv_id) {
+  try {
+    const sql = "SELECT * FROM public.inventory WHERE inv_id = $1";
+    const data = await pool.query(sql, [inv_id]);
+
+    return data.rows[0];
+  } catch (error) {
+    console.error("getInventoryById error: " + error);
+  }
+}
+
 /* ============================================================================ *
  * Exported Functions
  * ============================================================================ */
@@ -154,5 +165,6 @@ module.exports = {
   checkExistingClassification,
   getClassificationName,
   addNewInventoryItem,
-  checkExistingInventoryItem
+  checkExistingInventoryItem,
+  getInventoryById
 };
